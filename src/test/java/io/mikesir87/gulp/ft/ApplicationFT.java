d6b64c81bced52c8f73a9d15e7b314691fcd79ab
Michael Irwin <mikesir87@gmail.com>
2015-09-03 23:47:59 -0400

    Added Arquillian functional test

---
+++ src/test/java/io/mikesir87/gulp/ft/ApplicationFT.java
@@ -0,0 +1,61 @@
+package io.mikesir87.gulp.ft;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.InitialPage;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.Filters;
+import org.jboss.shrinkwrap.api.GenericArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ExplodedImporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.archive.importer.MavenImporter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import java.net.URL;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+/**
+ * A simple functional test, running in Arquillian.
+ *
+ * @author Michael Irwin
+ */
+@RunWith(Arquillian.class)
+public class ApplicationFT {
+
+  @Deployment(testable = false)
+  public static WebArchive generateDeployment() {
+    WebArchive archive = ShrinkWrap.create(MavenImporter.class)
+        .loadPomFromFile("pom.xml")
+        .importBuildOutput()
+        .as(WebArchive.class)
+        .addAsWebInfResource("test-jboss-web.xml", "jboss-web.xml");
+    archive.merge(
+        ShrinkWrap.create(GenericArchive.class).as(ExplodedImporter.class)
+            .importDirectory("target/gulp-webapp/gulp-demo-project").as(GenericArchive.class),
+        "/", Filters.include(".*")
+    );
+    return archive;
+  }
+
+  @Drone
+  private WebDriver browser;
+
+  @ArquillianResource
+  private URL deploymentUrl;
+
+  @Test
+  public void testRefreshButton(@InitialPage IndexPage indexPage) {
+    String timeDisplay = indexPage.assertOnPage().getTimeDisplay();
+    String newDisplay = indexPage.clickRefreshButton().getTimeDisplay();
+    assertThat(timeDisplay, is(not(equalTo(newDisplay))));
+  }
+
+}
---
+++ src/test/java/io/mikesir87/gulp/ft/IndexPage.java
@@ -0,0 +1,50 @@
+package io.mikesir87.gulp.ft;
+
+import com.google.common.base.Predicate;
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.page.Location;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.jboss.arquillian.graphene.Graphene.waitGui;
+
+/**
+ * A page fragment that represents the initial landing page
+ *
+ * @author Michael Irwin
+ */
+@Location("index.html")
+public class IndexPage {
+
+  @Drone
+  private WebDriver browser;
+
+  @FindBy(id = "current-time-display")
+  private WebElement timeDisplay;
+
+  @FindBy(id = "refresh-button")
+  private WebElement refreshButton;
+
+  public IndexPage assertOnPage() {
+    assertThat(browser.getTitle(), containsString("Sample Gulp Application"));
+    waitGui().withTimeout(2, TimeUnit.SECONDS).until()
+        .element(timeDisplay).is().present();
+    return this;
+  }
+
+  public String getTimeDisplay() {
+    return timeDisplay.getText();
+  }
+
+  public IndexPage clickRefreshButton() {
+    refreshButton.click();
+    waitGui().withTimeout(2, TimeUnit.SECONDS).until()
+        .element(refreshButton).is().visible();
+    return this;
+  }
+}
